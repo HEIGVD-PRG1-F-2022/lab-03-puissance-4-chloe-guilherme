60010b4dc1876329a3b698aec98cc84096e71f68
Salam <noname@hotmail.com>
2022-10-18 16:00:33 +0200

    fonction jouer

--- main.cpp
+++ main.cpp
@@ -17,21 +17,27 @@ Compilateur     : Mingw-w64 g++ 11.2.0
 
 using namespace std;
 
-void afficherTableau(const vector<vector<int>>tableau2D){
-    for(const vector<int>& tableau1D : tableau2D) {
+void afficherTableau(const vector<vector<int>>&tableau){
+    for(const vector<int>& tableau1D : tableau) {
         for (int val : tableau1D){
             cout << val << " ";
         }
         cout << endl;
     }
-
 }
+
+vector<vector<int>>jouer(vector<vector<int>>tableau, const int JOUEUR, int ligne, int colonne){
+    tableau.at(ligne).at(colonne) = JOUEUR;
+    return tableau;
+}
+
 
 
 int main() {
 
     vector<vector<int>>tableau(6, vector<int>(7, 0));
     afficherTableau(tableau);
+
 
 
     return EXIT_SUCCESS;
