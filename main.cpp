da2934489a1a94f6fa35903b5e3d2c29aaac47aa
GuilhermePintodac <guilherme.pintodacunhadamata@heig-vd.ch>
2022-10-18 15:21:25 +0200

    Init projet

--- main.cpp
+++ main.cpp
@@ -1,1 +1,21 @@
+/*
+-----------------------------------------------------------------------------------
+Nom du fichier  : main.cpp
+Auteur(s)       : Chloé Salamin,Guilherme Pinto
+Date creation   : 18.10.2022
+
+Description     : <à compléter>
+
+Remarque(s)     : <à compléter>
+
+Compilateur     : Mingw-w64 g++ 11.2.0
+-----------------------------------------------------------------------------------
+*/
+#include <cstdlib>
 #include <iostream>
+using namespace std;
+
+int main() {
+
+    return EXIT_SUCCESS;
+}
