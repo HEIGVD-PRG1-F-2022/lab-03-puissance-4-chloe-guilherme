c6f7a32f0e4301a9aa83c3cfec31a99c2d25cd7b
Salam <noname@hotmail.com>
2022-10-18 15:42:12 +0200

    Affichage tableau vide

--- main.cpp
+++ main.cpp
@@ -13,9 +13,26 @@ Compilateur     : Mingw-w64 g++ 11.2.0
 */
 #include <cstdlib>
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
+void afficherTableau(const vector<vector<int>>tableau2D){
+    for(const vector<int>& tableau1D : tableau2D) {
+        for (int val : tableau1D){
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+
+}
+
+
 int main() {
+
+    vector<vector<int>>tableau(6, vector<int>(7, 0));
+    afficherTableau(tableau);
+
 
     return EXIT_SUCCESS;
 }
