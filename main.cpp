aacb5ff11878e99e5f37279fb6e248317837bfab
Salam <noname@hotmail.com>
2022-10-19 17:22:41 +0200

    continuation fonction jouer

--- main.cpp
+++ main.cpp
@@ -27,19 +27,20 @@ void afficherTableau(const vector<vector<int>>&tableau){
 }
 
 vector<vector<int>> jouer(vector<vector<int>> &tableau, const int JOUEUR,
-                          int colonne, const int TAILLE_LIGNE, const int
-                          TAILLE_COLONNE) {
-   int ligne;
-   for (ligne = TAILLE_LIGNE; ligne > 0;) {
+                          int colonne, const int TAILLE_LIGNE) {
+
+    for (int ligne = TAILLE_LIGNE - 1; ligne >= 0;) {
       if(tableau.at(ligne).at(colonne) == 0){
          tableau.at(ligne).at(colonne) = JOUEUR;
+          return tableau;
       } else{
          ligne--;
       }
    }
    //tableau.at(ligne).at(colonne) = JOUEUR;
-   return tableau;
 }
+
+
 
 
 
@@ -47,13 +48,22 @@ int main() {
 
 
    //cin >>
-   const int JOUEUR1 = 1, JOUEUR2 = 2, TAILLE_LIGNE = 6, TAILLE_COLONNE = 7;
-   int ligne = 2, colonne = 5;
+   const int TAILLE_LIGNE = 6, TAILLE_COLONNE = 7;
+   int ligne = 2, colonne = 6, nombreCoup, joueur = 1;
    //vector<vector<int>> tableau(6, vector<int>(7, 0));
 
-   vector<vector<int>> tableau(TAILLE_LIGNE, vector<int>(TAILLE_COLONNE, 0));
-   jouer(tableau, JOUEUR1, ligne, colonne);
-   afficherTableau(tableau);
+
+   for ( nombreCoup =0; nombreCoup < TAILLE_LIGNE * TAILLE_COLONNE; ++nombreCoup ){
+
+       cout << "Joueur " << joueur << "\n Veuillez entrer le numéro de la colonne dans laquelle"
+                                   <<   "vous souhaitez jouer (1 - " << TAILLE_COLONNE << ") : ";
+       cin >> colonne;
+       vector<vector<int>> tableau(TAILLE_LIGNE, vector<int>(TAILLE_COLONNE, 0));
+       jouer(tableau, joueur, colonne, TAILLE_LIGNE);
+       afficherTableau(tableau);
+    }
+
+
 
    return EXIT_SUCCESS;
 }
