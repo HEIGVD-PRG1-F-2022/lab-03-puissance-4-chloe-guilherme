fc8a551eece87915ecd7a530fbfb4f682dc139cb
Salam <noname@hotmail.com>
2022-10-22 20:42:08 +0200

    Titre

--- main.cpp
+++ main.cpp
@@ -22,29 +22,38 @@ using namespace std;
 
 int main() {
 
-   cout << "******************" << endl
-        << "* " << "Regles du Jeu:" << " *" << endl
-        << "******************" << endl
-        << "Le but du jeu est d'aligner une suite de 4 pions de meme couleur sur "
-        << "une grille comptant 6 rangees et 7 colonnes. Chaque joueur dispose de "
-        << "21 pions d'une couleur (par convention, en general jaune ou rouge). "
-        << "Tour a tour, les deux joueurs placent un pion dans la colonne de leur "
-        << "choix, le pion coulisse alors jusqu'a la position la plus basse possible"
-        << " dans la dite colonne a la suite de quoi c'est a l'adversaire de jouer."
-        << " Le vainqueur est le joueur qui realise le premier un alignement "
-        << "(horizontal, vertical ou diagonal) consecutif d'au moins quatre "
-        << "pions de sa couleur. Si, alors que toutes les cases de la grille de"
-        << " jeu sont remplies, aucun des deux joueurs n'a realise un tel "
-        << "alignement, la partie est declaree nulle." << endl
-        << endl << endl
-        << "******************" << endl
-        << "* " << "Comment jouer:" << " *" << endl
-        << "******************" << endl
-        << "Ce programme va demander a tour de role au joueur 1 et au joueur 2 de"
-        << " jouer. Le joueur doit entrer le numero de la colonne dans laquelle il"
-        << " souhaite deposer un pion. Le pion joue est represente par un 1 pour "
-        << "le joueur 1 et un 2 pour le joueur 2. Les colonnes jouables sont 7 "
-        << " de 1 a 7." << endl << endl;
+    cout << "***************************************************************************" << endl
+         << "_______ ___ ___ ___ _______ _______ _______ ___ ___ _______ _______ ___ ___ " << endl
+         << "| ___ | | | | | | | | ____| | ____| | ___ | | | | | | ____| | ____| | | | | " << endl
+         << "| |_| | | | | | | | | |____ | |___  | |__|| |  || | | |     | |__   | |_| |" << endl
+         << "| ____| | | | | | | |____ | |____ | | ___ | |   | | | |     | ___|  |____ | " << endl
+         << "| |     | |_| | | |  ___| | ____| | | | | | | |__ | | |___  | |____     | | " << endl
+         << "|_|     |_____| |_| |_____| |_____| |_| |_| |_| |_| |_____| |_____|     |_| " << endl
+         << "*****************************************************************************" << endl
+         << endl << endl
+         << "******************" << endl
+         << "* " << "Regles du Jeu:" << " *" << endl
+         << "******************" << endl
+         << "Le but du jeu est d'aligner une suite de 4 pions de meme couleur sur "
+         << "une grille comptant 6 rangees et 7 colonnes. Chaque joueur dispose de "
+         << "21 pions d'une couleur (par convention, en general jaune ou rouge). "
+         << "Tour a tour, les deux joueurs placent un pion dans la colonne de leur "
+         << "choix, le pion coulisse alors jusqu'a la position la plus basse possible"
+         << " dans la dite colonne a la suite de quoi c'est a l'adversaire de jouer."
+         << " Le vainqueur est le joueur qui realise le premier un alignement "
+         << "(horizontal, vertical ou diagonal) consecutif d'au moins quatre "
+         << "pions de sa couleur. Si, alors que toutes les cases de la grille de"
+         << " jeu sont remplies, aucun des deux joueurs n'a realise un tel "
+         << "alignement, la partie est declaree nulle." << endl
+         << endl << endl
+         << "******************" << endl
+         << "* " << "Comment jouer:" << " *" << endl
+         << "******************" << endl
+         << "Ce programme va demander a tour de role au joueur 1 et au joueur 2 de"
+         << " jouer. Le joueur doit entrer le numero de la colonne dans laquelle il"
+         << " souhaite deposer un pion. Le pion joue est represente par un 1 pour "
+         << "le joueur 1 et un 2 pour le joueur 2. Les colonnes jouables sont 7 "
+         << " de 1 a 7." << endl << endl;
 
    const int TAILLE_LIGNE = 6, TAILLE_COLONNE = 7;
    int ligne, colonne, nombreCoup, joueur = 1;
